80df18ceab278107871c2c40902ffb8932b1e466
Yuting Wang <yutingw2@andrew.cmu.edu>
2024-01-20 21:25:44 -0500

    fix some errors by adding interfaces and modifying spaces

--- src/pagination.ts
+++ src/pagination.ts
@@ -1,9 +1,15 @@
-'use strict';
-
 import * as qs from 'querystring';
 import * as _ from 'lodash';
-// const qs = require('querystring');
-// const _ = require('lodash');
+
+interface PageData {
+    page: number;
+    active: boolean;
+    qs: string;
+}
+
+interface Separator {
+    separator: true;
+}
 
 interface PaginationData {
     prev: { page: number; active: boolean; qs: string };
@@ -11,29 +17,28 @@ interface PaginationData {
     first: { page: number; active: boolean; qs: string };
     last: { page: number; active: boolean; qs: string };
     rel: { rel: string; href: string }[];
-    pages: { page: number; active: boolean; qs: string }[];
+    pages: (PageData | Separator)[];
     currentPage: number;
     pageCount: number;
 }
 
-// const pagination = module.exports;
-const pagination: { create: 
-    (currentPage: number, pageCount: number, queryObj: Record<string, any>) 
-    => PaginationData } = {
-        create(currentPage, pageCount, queryObj) {
-            if (pageCount <= 1) {
-                return{
-                    prev: { page: 1, active: currentPage > 1, qs: '' },
-                    next: { page: 1, active: currentPage < pageCount, qs: '' },
-                    first: { page: 1, active: currentPage === 1, qs: '' },
-                    last: { page: 1, active: currentPage === pageCount, qs: '' },
-                    rel: [],
-                    pages: [],
-                    currentPage: 1,
-                    pageCount: 1,
-                };
-            }
-        //following things go here
+const pagination: {
+    create: (currentPage: number, pageCount: number, queryObj: Record<string, string | number>) => PaginationData;
+} = {
+    create(currentPage, pageCount, queryObj) {
+        if (pageCount <= 1) {
+            return {
+                prev: { page: 1, active: currentPage > 1, qs: '' },
+                next: { page: 1, active: currentPage < pageCount, qs: '' },
+                first: { page: 1, active: currentPage === 1, qs: '' },
+                last: { page: 1, active: currentPage === pageCount, qs: '' },
+                rel: [],
+                pages: [],
+                currentPage: 1,
+                pageCount: 1,
+            };
+        }
+
         pageCount = parseInt(String(pageCount), 10);
         let pagesToShow = [1, 2, pageCount - 1, pageCount];
 
@@ -45,31 +50,60 @@ const pagination: { create:
         if (startPage > pageCount - 5) {
             startPage -= 2 - (pageCount - currentPage);
         }
-        let i;
+        let i: number;
         for (i = 0; i < 5; i += 1) {
             pagesToShow.push(startPage + i);
         }
 
         pagesToShow = _.uniq(pagesToShow).filter(page => page > 0 && page <= pageCount).sort((a, b) => a - b);
-        
+
         queryObj = { ...(queryObj || {}) };
 
         delete queryObj._;
 
-        const pages = pagesToShow.map((page) => {
+        const pages: (PageData | Separator)[] = pagesToShow.map((page) => {
             queryObj.page = page;
-            return { page: page, active: page === currentPage, qs: qs.stringify(queryObj) };
+            return { page, active: page === currentPage, qs: qs.stringify(queryObj) } as PageData;
         });
 
         for (i = pages.length - 1; i > 0; i -= 1) {
-            if (pages[i].page - 2 === pages[i - 1].page) {
-            pages.splice(i, 0, { page: pages[i].page - 1, active: false, qs: qs.stringify(queryObj) });
-            } else if (pages[i].page - 1 !== pages[i - 1].page) {
-            pages.splice(i, 0, { separator: true });
+            if ((pages[i] as PageData).page - 2 === (pages[i - 1] as PageData).page) {
+                pages.splice(i, 0, {
+                    page: (pages[i] as PageData).page - 1,
+                    active: false,
+                    qs: qs.stringify(queryObj),
+                } as PageData);
+            } else if ((pages[i] as PageData).page - 1 !== (pages[i - 1] as PageData).page) {
+                pages.splice(i, 0, { separator: true } as Separator);
             }
         }
 
-        const data: PaginationData = { rel: [], pages, currentPage, pageCount };
+        const data: PaginationData = {
+            rel: [],
+            pages,
+            currentPage,
+            pageCount,
+            prev: {
+                page: 0,
+                active: false,
+                qs: '',
+            },
+            next: {
+                page: 0,
+                active: false,
+                qs: '',
+            },
+            first: {
+                page: 0,
+                active: false,
+                qs: '',
+            },
+            last: {
+                page: 0,
+                active: false,
+                qs: '',
+            },
+        };
         queryObj.page = previous;
         data.prev = { page: previous, active: currentPage > 1, qs: qs.stringify(queryObj) };
         queryObj.page = next;
@@ -95,7 +129,7 @@ const pagination: { create:
         }
 
         return data;
-        },
+    },
+};
 
-    };
 export default pagination;
