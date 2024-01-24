88f0d813e94c989175ae1510e672085a33253843
ytw-wyt <yutingw2@andrew.cmu.edu>
2024-01-24 16:47:20 -0500

    Found the problem of exporting! Now the code passes test but there is still gramma error for lint

--- src/pagination.ts
+++ src/pagination.ts
@@ -1,7 +1,32 @@
 import * as qs from 'querystring';
 import * as _ from 'lodash';
 
-interface PageData {
+export interface PaginationObject {
+    pagination: Pagination;
+}
+
+export interface Pagination {
+    prev: ActivePage;
+    next: ActivePage;
+    first: ActivePage;
+    last: ActivePage;
+    rel: Relation[];
+    pages: Page[];
+    currentPage: number;
+    pageCount: number;
+}
+
+export interface ActivePage {
+    page: number;
+    active: boolean;
+}
+
+export interface Relation {
+    rel: string;
+    href: string;
+}
+
+export interface Page {
     page: number;
     active: boolean;
     qs: string;
@@ -11,21 +36,21 @@ interface Separator {
     separator: true;
 }
 
-interface PaginationData {
-    prev: { page: number; active: boolean; qs: string };
-    next: { page: number; active: boolean; qs: string };
-    first: { page: number; active: boolean; qs: string };
-    last: { page: number; active: boolean; qs: string };
-    rel: { rel: string; href: string }[];
-    pages: (PageData | Separator)[];
+export interface PaginationData {
+    prev: ActivePage;
+    next: ActivePage;
+    first: ActivePage;
+    last: ActivePage;
+    rel: Relation[];
+    pages: (Page | Separator)[];
     currentPage: number;
     pageCount: number;
 }
-
 const pagination: {
     create: (currentPage: number, pageCount: number, queryObj: Record<string, string | number>) => PaginationData;
-} = {
-    create(currentPage, pageCount, queryObj) {
+} = module.exports;
+
+pagination.create = function (currentPage, pageCount, queryObj) {
         if (pageCount <= 1) {
             return {
                 prev: { page: 1, active: currentPage > 1, qs: '' },
@@ -50,6 +75,7 @@ const pagination: {
         if (startPage > pageCount - 5) {
             startPage -= 2 - (pageCount - currentPage);
         }
+
         let i: number;
         for (i = 0; i < 5; i += 1) {
             pagesToShow.push(startPage + i);
@@ -58,22 +84,21 @@ const pagination: {
         pagesToShow = _.uniq(pagesToShow).filter(page => page > 0 && page <= pageCount).sort((a, b) => a - b);
 
         queryObj = { ...(queryObj || {}) };
-
         delete queryObj._;
 
-        const pages: (PageData | Separator)[] = pagesToShow.map((page) => {
+        const pages: (Page | Separator)[] = pagesToShow.map((page) => {
             queryObj.page = page;
-            return { page, active: page === currentPage, qs: qs.stringify(queryObj) } as PageData;
+            return { page, active: page === currentPage, qs: qs.stringify(queryObj) } as Page;
         });
 
         for (i = pages.length - 1; i > 0; i -= 1) {
-            if ((pages[i] as PageData).page - 2 === (pages[i - 1] as PageData).page) {
+            if ((pages[i] as Page).page - 2 === (pages[i - 1] as Page).page) {
                 pages.splice(i, 0, {
-                    page: (pages[i] as PageData).page - 1,
+                    page: (pages[i] as Page).page - 1,
                     active: false,
                     qs: qs.stringify(queryObj),
-                } as PageData);
-            } else if ((pages[i] as PageData).page - 1 !== (pages[i - 1] as PageData).page) {
+                } as Page);
+            } else if ((pages[i] as Page).page - 1 !== (pages[i - 1] as Page).page) {
                 pages.splice(i, 0, { separator: true } as Separator);
             }
         }
@@ -86,33 +111,29 @@ const pagination: {
             prev: {
                 page: 0,
                 active: false,
-                qs: '',
             },
             next: {
                 page: 0,
                 active: false,
-                qs: '',
             },
             first: {
                 page: 0,
                 active: false,
-                qs: '',
             },
             last: {
                 page: 0,
                 active: false,
-                qs: '',
             },
         };
         queryObj.page = previous;
-        data.prev = { page: previous, active: currentPage > 1, qs: qs.stringify(queryObj) };
+        data.prev = { page: previous, active: currentPage > 1 };
         queryObj.page = next;
-        data.next = { page: next, active: currentPage < pageCount, qs: qs.stringify(queryObj) };
+        data.next = { page: next, active: currentPage < pageCount };
 
         queryObj.page = 1;
-        data.first = { page: 1, active: currentPage === 1, qs: qs.stringify(queryObj) };
+        data.first = { page: 1, active: currentPage === 1 };
         queryObj.page = pageCount;
-        data.last = { page: pageCount, active: currentPage === pageCount, qs: qs.stringify(queryObj) };
+        data.last = { page: pageCount, active: currentPage === pageCount };
 
         if (currentPage < pageCount) {
             data.rel.push({
@@ -129,8 +150,4 @@ const pagination: {
         }
 
         return data;
-    },
-};
-
-export default pagination;
-
+    };
